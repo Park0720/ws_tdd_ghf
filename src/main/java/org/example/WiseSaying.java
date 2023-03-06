4b5f413c4b2adb8605351fe6280e7d1923a44525
Park0720 <ppmm98@naver.com>
2023-03-06 15:35:30 +0900

    수명언 ì등록í 후 목록 표시

---
+++ src/main/java/org/example/WiseSaying.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class WiseSaying {
+    public long id;
+    public String content;
+    public String authorName;
+
+    public WiseSaying(long id, String content, String authorName) {
+        this.id = id;
+        this.content = content;
+        this.authorName = authorName;
+    }
+}
